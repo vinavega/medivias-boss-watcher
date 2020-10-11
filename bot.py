f41a0153b4eb80548ac65aa489bbb10cfbbd7862
Victor Navega <vinavega@gmail.com>
2020-10-11 01:21:20 -0300

    addd inicio e pausa via comando

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import schedule
 import time
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,22 +18,46 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    while True:
-        schedule.run_pending()
-        print("Executando verificação....")
-        for guild in client.guilds:
-            async for member in guild.fetch_members(limit=None):
-             if member.name == NAV:
-                break
-        servList = ['Legacy','Destiny','Pendulum','Prophecy','Purity']
-        mobsList = ['broodmother','archiona','arcestar','yeti','gieffrin','thousand eyes']
-        for servName in servList:
-          page = requests.get("https://mediviastats.info/recent-deaths.php?server="+ servName)
-          soup = BeautifulSoup(page.content, 'html.parser')
-          for mobName in mobsList:
-            a = soup(text=re.compile(mobName))
-            if a:
-               await member.send(mobName +" encontrado no "+ servName +" -> "+ str(a))
-        print("Execução finalizada, entrando em sleep")
-        time.sleep(30)
+    print("READY!")
+
+on = None
+
+@client.event
+async def on_message(message):
+    global on
+    channel = message.channel
+    if message.content.startswith('@start'):
+        if on:
+            await channel.send("O serviço já está ativado")
+        else:
+            await channel.send("Serviço iniciado")
+            on = True
+            print("Serviço iniciado")
+            while on:
+                print("Executando verificação....")
+                servList = ['Legacy']
+                mobsList = ['broodmother','archiona','arcestar','yeti','gieffrin','thousand eyes']
+                for servName in servList:
+                    page = requests.get("https://mediviastats.info/recent-deaths.php?server="+ servName)
+                    soup = BeautifulSoup(page.content, 'html.parser')
+                    for mobName in mobsList:
+                        a = soup(text=re.compile(mobName))
+                        if a:
+                            await channel.send(mobName +" encontrado no "+ servName +" -> "+ str(a))
+                print("Execução finalizada, entrando em sleep")
+                await asyncio.sleep(30)
+            
+    elif message.content.startswith('@stop'):
+        if not on:
+            await channel.send("O serviço já está desligado")
+        else:
+            print("Serviço pausado")
+            on = False
+            await channel.send("Serviço pausado")
+    elif message.content.startswith('@status'):
+        if on:
+            await channel.send("Serviço de monitoramento está rodando")
+        else:
+            await channel.send("Serviço de monitoramento desligado")
+    
 client.run(TOKEN)
